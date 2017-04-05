5f18a1ddfd9cd105dfb15791dd2fa92b5f8fdb02
Grace <gracefctorres@gmail.com>
2017-04-05 15:38:03 -0500

    colocando funcionalidad de paginas y organizacion de secciones

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,52 @@
+$(document).ready(init);
+var currentSection= null;
 
+function init()
+{
+    currentSection= $('#login');
+    $('#btn_inicio').click(onClickInicio);
+    $('#btn_accion').click(onClickAccion)
+    $('#btn_reportar').click(onClickReportar);
+    $('#btn_busca_ayuda').click(onClickBuscaAyuda);
+    $('#btn_tipo_ayuda').click(onClickTipoAyuda);
+    $('#btn_centros_ayuda').click(onClickCentroAyuda);
+    $('#btn_lista_desastres').click(onClickListDesastre);
+    /*$('#btn_info_desastres').click(onClickInfoDesastre);*/
+}
+function onClickInicio()
+{
+    goToSection('accion');
+}
+function onClickAccion()
+{
+    goToSection('reportar-desastre');
+}
+function onClickReportar()
+{
+    goToSection('buscar-ayuda');
+}
+function onClickBuscaAyuda()
+{
+    goToSection('tipo-ayuda');
+}
+function onClickTipoAyuda()
+{
+    goToSection('centros-ayuda');
+}
+function onClickCentroAyuda()
+{
+    goToSection('lista-desastres');
+}
+function onClickListDesastre()
+{
+    goToSection('info-desastres');
+}
+
+function goToSection(_id)
+{
+    currentSection.removeClass('visible');
+    var nextSection = $('#'+_id);
+    nextSection.addClass('visible'); 
+    currentSection=nextSection;
+    
+}
