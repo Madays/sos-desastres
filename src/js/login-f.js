ac764f8da879670e0409b47793e76343b3d249dd
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-04-06 11:23:58 -0500

    parse linked

--- src/js/login-f.js
+++ src/js/login-f.js
@@ -7,21 +7,98 @@ function statusChangeCallback(response) {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      testAPI();
+      saveUserFacebook();
+    
     } else {
       // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
     }
   }
+ 
 
+function saveUserFacebook(){
+    //web
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture'},
+    function (response) {
+        //alert('<p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>Locale:</b> '+response.locale+'</p><p><b>Picture:</b> <img src="'+response.picture.data.url+'"/></p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>');
+        //name: response.first_name + ' ' + response.last_name
+        //username: response.id
+        //email: response.email
+        //user_type: Estudiante
+        //school: ??
+        Parse.User.logIn(response.id, 'passwordFB:'+ response.id, {
+            success: function(results) {
+                UserLogin(results);
+            },
+            error: function (user, error) {
+                // The login failed. Check error to see why.
+                registrarUserFacebook(response);
+                log.console("Error: " + error.code + " " + error.message);
+            }
+        });
+    });
+}
+
+//si el usuario si esta en la base de datos
+function UserLogin(res){
+    sessionStorage.setItem('id', res.id);
+    sessionStorage.setItem('user_type', res.attributes.user_type);
+    sessionStorage.setItem('school', res.attributes.school);
+    sessionStorage.setItem('username', res.attributes.name);
+    sessionStorage.setItem('token', res.getSessionToken());
+    window.location = '/';
+}
+//si el usuario no esta en la base de datos
+function registrarUserFacebook(response){
+    console.log("register userFabeook");
+    var newuser = new Parse.User();
+    newuser.set("name", response.first_name + ' ' + response.last_name);
+    newuser.set("email", response.email);
+    newuser.set("username", response.id);
+    newuser.set("password", 'passwordFB:'+ response.id);
+    newuser.set("school", 'null');
+    newuser.set("user_type", "Estudiante");
+
+    newuser.signUp(null, {
+        success: function (res) {
+            UserLogin(res);
+        },
+        error: function (user, error) {
+            // Show the error message somewhere and let the user try again.
+            log.console("Error: " + error.code + " " + error.message);
+        }
+    });
+
+}
+
+function logout(){
+    sessionStorage.removeItem('id');
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('school');
+    sessionStorage.removeItem('user_type');
+    sessionStorage.removeItem('username');
+
+    /*FB.logout(function(response) {
+    // user is now logged out
+    });*/
+    FB.getLoginStatus(function(response) {
+        if (response && response.status === 'connected') {
+            FB.logout(function(response) {
+                // user is now logged out
+            });
+        }
+    });
+}
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
-    });
+    }, {scope:'email'});
+    //recupera el correo electronico con scope
   }
 
   window.fbAsyncInit = function() {
