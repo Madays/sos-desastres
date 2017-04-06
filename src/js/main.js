f021a7d7511c154c85e4bdaabde5e390814024b5
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-04-05 21:14:11 -0500

    avance maquetado form control

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,30 @@ function onClickInicio()
 function onClickAccion()
 {
     goToSection('reportar-desastre');
+    //var toTipoAyuda = $('#m-buscar');
+    //toTipoAyuda().hide();
+    
+    initMap();
+    
+    var btnBuscarAyuda = $("#btn_reportar");
+    
+    btnBuscarAyuda.click(showBuscarAyuda);    
 }
+
+function showBuscarAyuda(){
+    
+    var toBuscarAyuda = $('#d-info');
+    //var toTipoAyuda = $('#m-buscar');
+    
+    toBuscarAyuda.hide();
+    //toTipoAyuda.show();
+    
+    
+}
+
+
+
+
 function onClickReportar()
 {
     goToSection('buscar-ayuda');
@@ -48,5 +71,36 @@ function goToSection(_id)
     var nextSection = $('#'+_id);
     nextSection.addClass('visible'); 
     currentSection=nextSection;
+    
+}
+
+var map;
+function initMap() {
+    getLocation();                          
+}
+
+function getLocation() {
+   if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(showPosition);
+   } else {
+       alert("Geolocation is not supported by this browser.");
+   }
+}
+
+function showPosition(position) {
+   var myLatLng = {lat:position.coords.latitude,lon:position.coords.longitude};
+   map = new google.maps.Map(document.getElementById('map'), {
+   center: {lat: myLatLng.lat, lng: myLatLng.lon},
+   zoom: 16,
+    disableDefaultUI: true
+ });
+   
+   // Create a marker and set its position.
+   var marker = new google.maps.Marker({
+       map: map,
+       position: {lat: myLatLng.lat, lng: myLatLng.lon},
+       title: 'Hello World!'
+   });
+       
     
 }
